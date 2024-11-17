9ea94e2636f0f4558047473bbed4089d71868e7a
ola <olanazmy@gmail.com>
2024-11-17 21:02:42 +0200

    ApplicationDbContext, and first migration

---
+++ Full_EF_Practice/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer;
+
+namespace Full_EF_Practice
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        
+        options.UseSqlServer(@"Data Source=OLA;Initial Catalog=EF_FullPractice;Integrated Security=True;Trust Server Certificate=True;");
+
+            //add dbset
+            public DbSet<Employee> Employees { get; set; }
+    }
+ }
+
---
+++ Full_EF_Practice/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Full_EF_Practice
+{
+    public class Employee
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+    }
+}
